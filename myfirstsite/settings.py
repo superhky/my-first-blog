4506e69365171315fd62bdba54b6e81bc9dada7b
Superhky <superhky@gmail.com>
2020-02-08 17:26:01 +0900

    Changed host address.

--- myfirstsite/settings.py
+++ myfirstsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(n_p4amd4h%a#1fce-gkd-0t#c-h&vz(p$$cb$j6ejrrc#634y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
